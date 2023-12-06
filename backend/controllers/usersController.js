01124f3035da80cd08f9bd13a0f9fb1dc58cef30
drazod <ngtienkhanh1702@gmail.com>
2023-12-06 16:19:47 +0700

    99,5%

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -5,19 +5,18 @@ exports.signup = async (req, res) => {
   // console.log('Received POST request at /signup');
   try {
     // Extract user input from request body
-    const { email, password, age, gender, height, weight } = req.body;
+    const {firstName,lastName ,email, password,  gender, confirmPassword } = req.body;
 
     // Perform validation, sanitation, etc.
 
     // Create a new user instance with the provided data
     let newUser = new User({
       email: email,
-      // password: req.body.pass,
       gender: gender,
-      // height: req.body.height,
-      // weight: req.body.weight,
-      fullname: 'le Minh chanh',
+      fullname: firstName + lastName,
       birthdate: '2003-07-30',
+      password: password,
+      confirmPassword: confirmPassword,
       address: 'lmao lmao',
       phoneNum: '0835599955',
       history: {
@@ -26,16 +25,17 @@ exports.signup = async (req, res) => {
         weight: 100,
         activityPerWeek: 'seDentary',
       },
-      password: 'lmaolmao',
-      confirmPassword: 'lmaolmao',
     });
 
     // Save the new user to the database
     await newUser.save();
-    res.status(200).json({
-      status: 'success',
-      message: `Successfully signup with email: ${req.body.email}`,
-    });
+
+    // res.status(200).json({
+    //   status: 'success',
+    //   message: `Successfully signup with email: ${req.body.email}`,
+    // });
+    res.redirect('/home');
+
   } catch (err) {
     res.status(400).json({
       status: 'failed',
