b27bb77322efd3d2cee262a12b2cb3c4e2d57022
snipee3007 <leminhchanh2003@gmail.com>
2025-02-25 18:39:08 +0700

    refracturing code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,7 @@ module.exports = {
   content: ['./frontend/**/*.{pug,html,js}'],
   theme: {
     extend: {
-      fontFamily: {
-        Fustat: ['Fustat'],
-        Roca: ['Roca'],
-        'DM-Sans': ['DM-Sans'],
-        Urbanist: ['Urbanist'],
-        Roboto: ['Roboto'],
-        Inter: ['Inter'],
-        ABeeZee: ['ABeezee'],
-        AbhayaLibre: ['AbhayaLibre'],
-      },
+      fontFamily: {},
     },
   },
   plugins: [],
