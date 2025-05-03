b6e3c8267ba0c298df36dab57cda6190f60381d7
Ittoday <lethienan20012003@gmail.com>
2025-05-03 23:34:42 +0700

    fix appointment

--- backend/routers/api/doctorRoutes.js
+++ backend/routers/api/doctorRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const doctorController = require('./../../controllers/doctorController.js');
 const template = require('./../../controllers/templateController.js');
+const authController = require('./../../controllers/authController.js');
 
 const router = express.Router();
 
-router.get('/', doctorController.getDoctors);
+router.get('/', authController.isSignedIn, doctorController.getDoctors);
 
 router.route('/:name').get(template.getDoctorItemTemplate);
 
