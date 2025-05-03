8e5cd5f4bd1a8f2501438899fe1394150a4e0397
snipee3007 <leminhchanh2003@gmail.com>
2025-05-03 18:55:56 +0700

    Update on reset password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,9 @@ const catchAsync = require('./../utils/catchAsync.js');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/users_schema');
 const AppError = require('./../utils/appError.js');
-
+const crypto = require('crypto');
+const speakeasy = require('speakeasy');
+const Email = require('../utils/email.js');
 const signToken = (id) => {
   // console.log(process.env.JWT_EXPIRES_IN);
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -192,6 +194,50 @@ exports.signOut = catchAsync(async (req, res, next) => {
   res.status(204).json();
 });
 
+exports.getOTP = catchAsync(async function (req, res, next) {
+  const data = req.body;
+  const acc = await User.findOne({ email: data.email });
+  if (acc) {
+    return next(
+      new AppError(
+        'Email người dùng này đã tồn tại! Vui lòng sử dụng email khác!'
+      )
+    );
+  }
+  if (req.user) {
+    res.writeHead(302, 'Already Login! You will be redirect to homepage!', {
+      location: '/',
+    });
+    res.end();
+  }
+  try {
+    let otp = speakeasy.totp({
+      secret: process.env.OTP_SECRET,
+      encoding: 'base64',
+      algorithm: 'sha256',
+      step: process.env.OTP_EXPIRES_IN,
+      window: 10,
+    });
+    const user = {
+      otp,
+      email: data.email,
+    };
+    await new Email(user).sendOTP();
+    res.status(200).json({
+      status: 'success',
+      message: `Đã gửi mã OTP qua email ${data.email} thành công`,
+    });
+  } catch (err) {
+    console.log(err);
+    return next(
+      new AppError(
+        'Có lỗi xảy ra trong việc gửi email! Vui lòng thử lại sau!',
+        500
+      )
+    );
+  }
+});
+
 exports.restrictTo = (role) => {
   return (req, res, next) => {
     let authenticate = false;
@@ -235,3 +281,121 @@ exports.restrictToAPI = (role) => {
     }
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(
+      new AppError(
+        'There is no user that have this email in our database! Please use another email!.',
+        404
+      )
+    );
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await User.findByIdAndUpdate(
+    user.id,
+    {
+      passwordResetToken: user.passwordResetToken,
+      passwordResetExpires: user.passwordResetExpires,
+    },
+    { runValidators: true }
+  );
+
+  // 3) Send it to user's email
+  try {
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
+    res.status(200).json({
+      status: 'success',
+      message: 'Token has been sent through email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    console.log(err);
+    return next(
+      new AppError('There is a error when sending email! Please try again!'),
+      500
+    );
+  }
+});
+
+exports.checkResetToken = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    res.writeHead(302, 'Token expired!', { location: '/' });
+    res.end();
+  } else {
+    next();
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+
+  user.password = req.body.password;
+  user.confirmPassword = req.body.confirmPassword;
+
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  // 3) Update changedPasswordAt property for the user
+  user.changedPasswordAt = Date.now();
+
+  // 4) Log the user in, send JWT
+  await user.save();
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.changedPasswordAt = Date.now();
+
+  await user.save();
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, res);
+});
---
+++ frontend/src/forgotPasswordController.js
@@ -0,0 +1,57 @@
+import Loader from './utils/loader.js';
+import { renderPopup } from './utils/popup.js';
+
+class ForgotPassword {
+  constructor() {
+    this.#forgotPassword();
+    this.#deleteIcon();
+  }
+  #deleteIcon() {
+    document
+      .querySelector('.deleteValue')
+      .addEventListener('click', function (e) {
+        if (e.target.closest('.deleteValue')) {
+          document.querySelector('input#email').value = '';
+        }
+      });
+  }
+  #forgotPassword() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const email = document.querySelector('input[name="forgotEmail"]');
+      console.log(email);
+      if (email.value) {
+        forgotPassword({ email: email.value });
+      }
+    });
+  }
+}
+
+const forgotPassword = async function (data) {
+  try {
+    Loader.create();
+    const res = await axios({
+      method: 'post',
+      url: 'api/user/forgotPassword',
+      data,
+    });
+    if (res.data.status == 'success') {
+      renderPopup(
+        res.status,
+        'Send reset email',
+        'Reset password email has been sent! Check your inbox or spam to reset your account password!'
+      );
+    }
+    Loader.destroy();
+  } catch (err) {
+    Loader.destroy();
+    renderPopup(
+      err.response.status,
+      'Send reset email',
+      err.response.data.message
+    );
+  }
+};
+
+new ForgotPassword();
