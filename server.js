0c807d92ed1bfd2c80e1eb2444f4260827f6c952
snipee3007 <leminhchanh2003@gmail.com>
2025-05-04 16:32:31 +0700

    update on server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import serverless from 'serverless-http';
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
@@ -13,6 +14,8 @@ const DB = process.env.DATABASE.replace(
 // console.log(DB); //MUST CHANGE THE CONFIG, ADD cohealth before ?
 mongoose.connect(DB).then(() => console.log('Connect to database successful'));
 
-server.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+export const handler = serverless(server);
+
+// server.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
