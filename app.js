ce7cec1d89fc8e71ea1f3053b9cf7fd27b532587
snipee3007 <leminhchanh2003@gmail.com>
2025-05-19 19:15:10 +0700

    update on rate limit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const globalErrorHandler = require('./backend/controllers/errorController.js');
 // Limit number of access
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
